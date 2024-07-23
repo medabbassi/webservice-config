d7182a14f8ee48fd208507f9957d921e32a85285
medabbassiEsprit <mabbassix3@gmail.com>
2024-07-24 00:12:03 +0100

    erueka and cing server

---
+++ Serveur-eureka/src/main/java/com/example/Serveureureka/ServeurEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.example.Serveureureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServeurEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurEurekaApplication.class, args);
+	}
+
+}
